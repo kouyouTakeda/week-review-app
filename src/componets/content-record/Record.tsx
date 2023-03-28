feb0386f09aa2856fa2259429103c7b26ae3b7fc
kouyouTakeda <kouyou.pc1202@gmail.com>
2023-03-28 11:29:23 +0900

    #3 ルーティングの完了

--- src/componets/content-record/Record.tsx
+++ src/componets/content-record/Record.tsx
@@ -1,39 +1,91 @@
 import type React from 'react';
+import { useState } from 'react';
 import Edit from '../edit/Edit';
 import './Record.scss';
 
 const Record: React.FC = () => {
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [selectedAnime, setSelectedAnime] = useState<anime>();
+
+  // 仮データハードコーディング
+  type animeList = {
+    animes: anime[];
+  };
+  type anime = {
+    title: string;
+    schedule: string;
+    tags: string[];
+    reviews: review[];
+    img: string;
+  };
+  type review = {
+    storyNumber: number;
+    data: string;
+  };
+
+  const samplereview: review = {
+    storyNumber: 1,
+    data: '感想感想感想感想感想感想感感想感想感想感想',
+  };
+  const sampleanime: anime = {
+    title: 'タイトル',
+    schedule: Date(),
+    tags: ['青春', 'バトル', '学園'],
+    reviews: [samplereview, samplereview],
+    img: '../../../public/img/damy-keyvisual.png',
+  };
+  const sampleanimeList: animeList = {
+    animes: [sampleanime, sampleanime, sampleanime],
+  };
+
   return (
     <div className="record">
       <div className="record-left">
-        <p className="record-title">タイトル</p>
-        <p className="record-title">タイトル</p>
-        <p className="record-title">タイトル</p>
+        {sampleanimeList.animes.map((anime, index) => {
+          return (
+            <p
+              className="record-title"
+              key={index}
+              aria-hidden="true"
+              onClick={() => {
+                setSelectedAnime(anime);
+              }}
+            >
+              {anime.title}
+            </p>
+          );
+        })}
       </div>
 
       <div className="record-right">
         <div className="record-menu">
           <button>Add</button>
         </div>
-        <div className="record-episode">
-          <h2 className="episode-number">1話</h2>
-          <div className="record-episode-text">
-            <p>
-              感想感想感想感想感想感想感感想感想感想感想感想
-              感想感感想感想感想感想感想感想感感想感想感想感
-              想感想感想感想感感想感想感想感想感想感想感感想
-              感想感想感想感想感想感感想感想感想感想感想感想
-              感想感感想感想感想感想感想感想感感想感想感想感
-            </p>
+        {selectedAnime?.reviews.map((review) => (
+          <div className="record-episode" key={review.storyNumber}>
+            <h2 className="episode-number">{review.storyNumber}話</h2>
+            <div className="record-episode-text">
+              <p>{review.data}</p>
+            </div>
+            <div className="record-episode-bottons">
+              <button
+                onClick={() => {
+                  setIsEdit((isEdit) => !isEdit);
+                }}
+              >
+                edit
+              </button>
+              <button>view</button>
+            </div>
           </div>
-          <div className="record-episode-bottons">
-            <button>eidt</button>
-            <button>view</button>
-          </div>
-        </div>
+        )) ?? (
+          <h3 className="record-null">
+            感想が無い、もしくはアニメが選択されていません
+          </h3>
+        )}
       </div>
 
-      <Edit />
+      {isEdit && <Edit />}
     </div>
   );
 };
